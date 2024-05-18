902811df618fb6c3c2376a659f5bd5edf380837f
nonelone <nonelpublic@proton.me>
2024-05-18 19:40:34 +0200

    Added packages.lua containing package lists

---
+++ packages.lua
@@ -0,0 +1,16 @@
+UX_packages = {
+    'posy-cursors',
+    'papirus-icon-theme'
+}
+
+Programming_packages = {
+    'vscodium-bin',
+    'dart',
+    'python',
+    'rustup'
+}
+
+Tool_packages = {
+    'VLC',
+    'neofetch'
+}
